5ad2f577cfd5d16b88f593c0705a4c6e7416c8ed
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-07-16 22:23:48 +0530

    Add files via upload

---
+++ break and continue.py
@@ -0,0 +1,12 @@
+for i in range(1,12):
+    if (i==11):
+        break # break statement
+    print("5 X" ,i, "=",5*i)
+    
+# continue statements
+j=0
+while(j<=10):
+    if (j==5):
+        continue
+    print(j)
+    j=j+1
